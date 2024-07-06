adf24e94f1ee5fb98714905536a0014adcd252f4
phthallo <84078890+phthallo@users.noreply.github.com>
2024-07-06 20:33:07 +0930

    chore: bump version number

--- src/pyflagoras/__init__.py
+++ src/pyflagoras/__init__.py
@@ -3,5 +3,5 @@ import json
 from importlib.resources import files
 
 flag_aliases_list = "\n".join((json.loads(files("pyflagoras").joinpath("flag_aliases.json").read_text(encoding="utf-8"))).keys())
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __list__ = flag_aliases_list
